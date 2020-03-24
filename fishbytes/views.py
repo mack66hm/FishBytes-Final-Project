585ae4c18d3900d76afa534f64807a39fcfd7c2e
DeeMeyers <justdmeyers@gmail.com>
2020-03-24 14:29:18 -0400

    basic lakedetail veiw made

--- fishbytes/views.py
+++ fishbytes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Lakes
 
 # Create your views here.
 def homepage(request):
     return render(request, 'base.html')
+
+def lakedetail(request, pk):
+    lake = Lakes.objects.get(pk=pk)
+    fish = lake.fish_in_lake
+    return render(request, 'core/lakedetail.html', {'lake': lake, 'fish': fish, })
