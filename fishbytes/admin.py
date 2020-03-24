dee0bed5cbf2f966f5ef0958693341ef42ce1c84
mack66hm <mack66hm@gmail.com>
2020-03-24 17:32:03 -0400

    added reg model

--- fishbytes/admin.py
+++ fishbytes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from fishbytes.models import Lakes, Fish
+from fishbytes.models import Lakes, Fish, Regulations
 
 # Register your models here.
 admin.site.register(Lakes)
 admin.site.register(Fish)
+admin.site.register(Regulations)
