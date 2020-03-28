c3a71efdba3857facd0f784deeea8169a29db3e2
DeeMeyers <justdmeyers@gmail.com>
2020-03-28 16:51:55 -0400

    question model added to admin, merges successful

--- fishbytes/admin.py
+++ fishbytes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from fishbytes.models import Lake, Fish, Regulation, Catch, Tag
+from fishbytes.models import Lake, Fish, Regulation, Catch, Tag, Question
 
 # Register your models here.
 admin.site.register(Lake)
@@ -7,3 +7,4 @@ admin.site.register(Fish)
 admin.site.register(Regulation)
 admin.site.register(Catch)
 admin.site.register(Tag)
+admin.site.register(Question)
