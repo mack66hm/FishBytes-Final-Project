7700bb60f8a8bb768f8534fd9c32743a9cc07676
DeeMeyers <justdmeyers@gmail.com>
2020-03-30 22:54:36 -0400

    show and hide works right now

--- static/identify.js
+++ static/identify.js
@@ -1,1 +1,9 @@
-const buttons = document.querySelectorAll(".b")
+const buttons = document.querySelectorAll(".button")
+
+for(let button of buttons){
+    button.addEventListener('click', function(e){
+        let next = button.id;
+        button.parentElement.classList.remove('active');
+        document.querySelector(`#tab-${next}`).classList.add('active');
+    });
+}
