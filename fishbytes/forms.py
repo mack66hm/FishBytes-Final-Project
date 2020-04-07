1300ff263aa73ce6e144953cbf6a636e2257c522
Geoff <gabishop35@gmail.com>
2020-04-06 22:36:38 -0400

    make some minor changes to the form and add black crappie to id pics

--- fishbytes/forms.py
+++ fishbytes/forms.py
@@ -8,6 +8,9 @@ from fishbytes.models import Catch
 
 
 class CatchForm(forms.ModelForm):
+    size = forms.CharField(label="Size (inches):")
+    weight = forms.CharField(label="Weight (lbs):")
+    date = forms.DateField(label="Date (MM/DD/YYYY):")
     class Meta:
         model = Catch
         fields = ('image', 'fish', 'size', 'weight', 'lake', 'date', 'longitude', 'latitude',)
